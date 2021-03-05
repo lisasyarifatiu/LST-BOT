ae2824fa7a922eb942e39c0e3213e51ff34d67c5
lisasyarifatiu <79804161+lisasyarifatiu@users.noreply.github.com>
2021-03-05 13:44:48 +0700

    Add files via upload

---
+++ src/iklan.js
@@ -0,0 +1,28 @@
+const iklan1 = (pushname, prefix, botName, ownerName) => { 
+	return `🔰 -----[ *「 IKLAN ${botName} 」* ]----- 🔰
+──────────────────────────────
+◯ *DAFTAR SEWA & BUAT BOT :*
+◯ *SEWA : 25K/GRUP (BULAN)*
+◯ *BUAT : 100K (BISA JADI OWNER)*
+◯ *PEMBAYARAN BISA MELALUI :*
+◯ *OVO, GOPAY, DANA, PULSA+10K*
+──────────────────────────────
+◯ *KEUNTUNGAN SEWA BOT :*
+◯ *1. BISA MEMASUKAN BOT KE GROUP*
+◯ *2. BISA MENGGUNAKAN FITUR PREMIUM*
+◯ *KEUNTUNGAN BUAT BOT :*
+◯ *1. BISA MENJADI OWNER BOT SENDIRI*
+◯ *2. BISA MENGGANTI NAMA BOT SENDIRI*
+◯ *3. BISA MEMBAWA BOT KE GROUP*
+◯ *4. BISA MENGGUNAKAN COMMAND OWNER*
+◯ *5. BISA MENYEWAKAN BOT KEMBALI*
+──────────────────────────────
+◯ *JIKA MINAT IKLAN DIATAS*
+◯ *HARAP HUBUNGI NOMOR DIBAWAH :*
+◯ *wa.me/mautaunonya:v*
+──────────────────────────────
+🔰 -----[ *「 POWERED BY ${LISA} 」* ]----- 🔰
+`
+}
+
+exports.iklan1 = iklan1
